5e0cbfd1885aa7fd480c8fe12b70d8f3dec0d40a
Ahmet Furkan AYLAÇ <58889821+innery@users.noreply.github.com>
2024-05-27 18:56:15 +0300

    Supabase bağlantısı
    Supabase connection kodlarının girilmesi ve import edilmesi

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,12 @@ import 'package:coinpro_prokit/utils/CPDataProvider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:nb_utils/nb_utils.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 AppStore appStore = AppStore();
 
 void main() async {
+  
   WidgetsFlutterBinding.ensureInitialized();
 
   await initialize(aLocaleLanguageList: languageList());
@@ -19,7 +21,20 @@ void main() async {
   defaultToastGravityGlobal = ToastGravity.BOTTOM;
 
   runApp(const MyApp());
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    url: 'https://wrzngalxdfmfojbtcugy.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indyem5nYWx4ZGZtZm9qYnRjdWd5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTYzNTkzODIsImV4cCI6MjAzMTkzNTM4Mn0.0JzveiHtC7zZVL8mTVBKwjlTuyr1tWH5KckE_h5ZxiY',
+  );
+
+  runApp(MyApp());
 }
+
+// It's handy to then extract the Supabase client in a variable for later uses
+final supabase = Supabase.instance.client;
+
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
